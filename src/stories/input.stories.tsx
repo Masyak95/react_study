a240f040fc6ff4d359a5b8cf780ca96aa02b29ea
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-04-12 17:48:22 +0200

    input flux

---
+++ src/stories/input.stories.tsx
@@ -0,0 +1,51 @@
+import React, {ChangeEvent, useRef, useState} from "react";
+import {action} from "@storybook/addon-actions";
+
+
+export default {
+    title: "input"
+}
+
+export const UncontrolledInput = () => <input/>
+export const TrackValueOfUncontrolledInput = () => {
+
+    const [value, setValue] = useState("")
+    const onchange = (event: ChangeEvent<HTMLInputElement>)=>{
+        const actualValue = event.currentTarget.value
+        setValue(actualValue)
+    }
+    return <><input onChange={onchange}/> = {value}</>
+}
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+
+    const [value, setValue] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null)//когда компонента только отрисосываеться нет ссылки поэтому null
+    const save = ()=>{
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+
+
+    return <><input ref={inputRef}/> <button onClick={save}>save</button> = actual value :{value}</>
+}
+
+//_______________________________________________________________________________________________
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState("")
+    const onChange = (e: ChangeEvent<HTMLInputElement>)=>{setParentValue(e.currentTarget.value)}
+
+
+    return <input value={parentValue} onChange={onChange}/>
+
+}
+export const ControlledCheckbox = () => {
+
+}
+
+export const ControlledSelect = () => {
+
+}
+
+
+export const ControlledInputWithFixedValue = () => <input value={"it-incubator"}/>
