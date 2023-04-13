9a8699da68d602727501051a7bdb3ed748000db6
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-04-13 11:10:48 +0200

    input flux continue

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -34,16 +34,24 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 export const ControlledInput = () => {
     const [parentValue, setParentValue] = useState("")
     const onChange = (e: ChangeEvent<HTMLInputElement>)=>{setParentValue(e.currentTarget.value)}
-
-
     return <input value={parentValue} onChange={onChange}/>
 
 }
 export const ControlledCheckbox = () => {
-
+    const [parentValue, setParentValue] = useState(true)
+    const onChange = (e: ChangeEvent<HTMLInputElement>)=>{setParentValue(e.currentTarget.checked)}
+    return <input type={"checkbox"} checked={parentValue} onChange={onChange}/>
 }
 
 export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>("2")
+    // const onChange = (e: ChangeEvent<HTMLInputElement>)=>{setParentValue(e.currentTarget.checked)}
+    return <select value={parentValue}>
+        <option>none</option>
+        <option value={"1"}>Minsk</option>
+        <option value={"2"}>Praha</option>
+        <option value={"3"}>Berlin</option>
+    </select>
 
 }
 
