a0c102c5fd0067732035c0e7012bda4792696c59
Maryia <97767940+Masyak95@users.noreply.github.com>
2023-04-19 14:18:59 +0200

    react.memo

--- src/components/OnOff/OnOf.tsx
+++ src/components/OnOff/OnOf.tsx
@@ -1,13 +1,14 @@
-import {useState} from "react";
+
 
 type PropsType = {
-    // on: boolean
+    on: boolean,
+    onChange: (on: boolean)=> void
 }
 
 
-function OnOff (props: PropsType){
+function OnOff ({on, onChange}:PropsType){
+
     console.log("App rendering")
-   let [on, setOn] = useState(false);
 
     const onStyle = {
         width: "30px",
@@ -38,8 +39,8 @@ function OnOff (props: PropsType){
 
     return (
         <div>
-            <div style={onStyle} onClick={ ()=> { setOn(true)}}>On</div>
-            <div style={offStyle} onClick={ ()=> { setOn(false)}}>Off</div>
+            <div style={onStyle} onClick={ ()=> { onChange(true)}}>On</div>
+            <div style={offStyle} onClick={ ()=> { onChange(false)}}>Off</div>
             <div style={indicatorStyle}></div>
         </div>
     )
